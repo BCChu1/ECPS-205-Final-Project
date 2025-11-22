e03330dbcb376e1a9a2dd44bf0999a617c8e6e6a
Bradley Chu <bradleycchu@gmail.com>
2025-11-21 21:55:02 -0800

    Added support for SPO2, HRSTD, and RMSSD for client and server

--- BlueToothServer.py
+++ BlueToothServer.py
@@ -4,6 +4,7 @@ import constants
 import time
 from typing import Any
 from max30102.heartrate_monitor import HeartRateMonitor
+import numpy as np
 
 from bless import (  # type: ignore
     BlessServer,
@@ -41,6 +42,17 @@ for cmd in commands:
 # SERVICE_UUID = "7436b48e-96ed-4b8f-916d-1f1d25964635"
 # CHAR_UUID = "74578b1f-1846-4759-ab5a-317cfa5ba6c9"
 
+def calculateSTD(lst : list[int]) -> float:
+    return np.std(lst)
+
+def calculateRMSSD(lst : list[int]) -> float:
+    if len(lst) == 0:
+        return 0
+    time_between_heartbeats = [60000/bpm for bpm in lst]       #BPM * 1min/60 seconds = BPS, invert to get seconds between each beat, convert to milliseconds
+    successive_differences = np.diff(time_between_heartbeats)   #successive differences
+    mean_squared_differences = np.mean(successive_differences ** 2) #mean square of succ. diff.
+    return np.sqrt(mean_squared_differences)
+    
 def read_request(characteristic: BlessGATTCharacteristic, **kwargs) -> bytearray:
     """Handle read requests"""
     print(f"Read request for {characteristic.uuid}")
@@ -50,6 +62,11 @@ def write_request(characteristic: BlessGATTCharacteristic, value: Any, **kwargs)
     """Handle write requests"""
     print(f"Write request for {characteristic.uuid}: {value}")
     characteristic.value = value
+
+def update_characteristic(server : BlessServer, char : str, newValue, valueName : str):
+    print(f"Updating {valueName} value to: {newValue:.2f}")
+    server.get_characteristic(char).value = bytearray(f"{newValue:.2f}", 'utf-8')
+    server.update_value(constants.SERVICE_UUID, char)
 
 async def run_server_with_custom_ads():
     # Custom advertisement data
@@ -69,13 +86,14 @@ async def run_server_with_custom_ads():
         | GATTCharacteristicProperties.indicate
     )
     permissions = GATTAttributePermissions.readable
-    await server.add_new_characteristic(
-        constants.SERVICE_UUID,
-        constants.CHARACTERISTIC_UUID,
-        char_flags,
-        None,
-        permissions
-    )
+    for characteristic in constants.CHARACTERISTIC_LIST:
+        await server.add_new_characteristic(
+            constants.SERVICE_UUID,
+            characteristic,
+            char_flags,
+            None,
+            permissions
+        )
 
     await server.start()
 
@@ -86,6 +104,7 @@ async def run_server_with_custom_ads():
     # Initialize the HeartRateMonitor object
     # Set print_raw to True to avoid printing raw data
     # Set print_result to True to print the calculated results
+    BPM_Samples = []
     try:
         print("Starting heart rate sensor...")
         # Start the heart rate sensor
@@ -93,9 +112,19 @@ async def run_server_with_custom_ads():
         while True:
             # Update the characteristic value with the current BPM
             BPM = hrm.bpm
-            print(f"Updating BPM value to: {BPM:.2f}")
-            server.get_characteristic(constants.CHARACTERISTIC_UUID).value = bytearray(f"{BPM:.2f}", 'utf-8')
-            server.update_value(constants.SERVICE_UUID, constants.CHARACTERISTIC_UUID)
+            SPO2 = hrm.sp02
+            BPM_Samples.append(BPM)
+            STD = calculateSTD(BPM_Samples)
+            RMSSD = calculateRMSSD(BPM_Samples)
+
+            # print(f"Updating BPM value to: {BPM:.2f}")
+            # server.get_characteristic(constants.CHARACTERISTIC_UUID).value = bytearray(f"{BPM:.2f}", 'utf-8')
+            # server.update_value(constants.SERVICE_UUID, constants.CHARACTERISTIC_UUID)
+            update_characteristic(server, constants.BPM_CHARACTERISTIC_UUID, BPM, "BPM")
+            update_characteristic(server, constants.SPO2_CHARACTERTISTIC_UUID, SPO2, "SPO2")
+            update_characteristic(server, constants.HRSTD_CHARACTERISTIC_UUID, STD, "HRSTD")
+            update_characteristic(server, constants.RMSSD_CHARACTERISTIC_UUID, RMSSD, "RMSSD")
+
             await asyncio.sleep(1)
     except KeyboardInterrupt:
         # Stop the sensor after keyboard interrupt
